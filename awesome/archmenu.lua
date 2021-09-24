a4b5a9b0fc1ef3777bf6b98331ca3d6181215d15
niel2460 <niel2460@hotmail.com>
2021-09-24 12:11:17 -0600

    actualizaciones de widgets awesome

---
+++ awesome/archmenu.lua
@@ -0,0 +1,80 @@
+ local menuf9c59cf15f7e3fd54a418134ad8a8be6 = {
+     {"Code - OSS", "/usr/bin/code-oss --unity-launch ", "/usr/share/pixmaps/com.visualstudio.code.oss.png" },
+     {"Galculator", "galculator", "/usr/share/icons/hicolor/48x48/apps/galculator.png" },
+     {"KeePass Password Safe", "keepass", "/usr/share/icons/hicolor/16x16/apps/keepass.png" },
+     {"Vim", "xterm -e vim ", "/usr/share/icons/hicolor/48x48/apps/gvim.png" },
+     {"Visor de imágenes", "gpicview ", "/usr/share/icons/hicolor/48x48/apps/gpicview.png" },
+     {"Xarchiver", "xarchiver ", "/usr/share/icons/hicolor/16x16/apps/xarchiver.png" },
+     {"compton", "compton", "/usr/share/icons/hicolor/48x48/apps/compton.png" },
+     {"nitrogen", "nitrogen", "/usr/share/icons/hicolor/16x16/apps/nitrogen.png" },
+     {"picom", "picom"},
+ }
+
+ local menue2fa0f6ba17f6bf3d1bf1dbed5a1c5b2 = {
+     {"Inkscape", "inkscape ", "/usr/share/icons/hicolor/16x16/apps/org.inkscape.Inkscape.png" },
+     {"Visor de imágenes", "gpicview ", "/usr/share/icons/hicolor/48x48/apps/gpicview.png" },
+ }
+
+ local menu010ccd73d8965edb946094904b05ace3 = {
+     {"Alacritty", "alacritty", "/usr/share/pixmaps/Alacritty.svg" },
+     {"BleachBit", "bleachbit", "/usr/share/pixmaps/bleachbit.png" },
+     {"Cajas", "gnome-boxes "},
+     {"Configuración del sistema", "io.elementary.switchboard "},
+     {"GParted", "/usr/bin/gparted ", "/usr/share/icons/hicolor/16x16/apps/gparted.png" },
+     {"Gestor de archivos PCManFM", "pcmanfm "},
+     {"Hardware Locality lstopo", "lstopo"},
+     {"Htop", "xterm -e htop", "/usr/share/pixmaps/htop.png" },
+     {"LSHW", "/usr/sbin/gtk-lshw", "///usr/share/lshw/artwork/logo.svg" },
+     {"MultiWriter", "gnome-multi-writer", "/usr/share/icons/hicolor/16x16/apps/org.gnome.MultiWriter.png" },
+     {"Navegador Zeroconf de Avahi", "/usr/bin/avahi-discover"},
+     {"Octopi", "/usr/bin/octopi", "/usr/share/icons/octopi.png" },
+     {"Octopi CacheCleaner", "/usr/bin/octopi-cachecleaner", "/usr/share/icons/octopi.png" },
+     {"ranger", "xterm -e ranger"},
+ }
+
+ local menuc8205c7636e728d448c2774e6a4a944b = {
+     {"AnyDesk", "/usr/bin/anydesk ", "/usr/share/pixmaps/anydesk.svg" },
+     {"Examinador de servidores SSH de Avahi", "/usr/bin/bssh"},
+     {"Examinador de servidores VNC de Avahi", "/usr/bin/bvnc"},
+     {"Opera", "opera ", "/usr/share/icons/hicolor/16x16/apps/opera.png" },
+     {"Zoom", "/usr/bin/zoom ", "/usr/share/pixmaps/Zoom.png" },
+ }
+
+ local menubd41283f290f7ee66eb3f4601b70a182 = {
+     {"WPS 2019", "/usr/bin/wps ", "/usr/share/icons/hicolor/16x16/apps/wps-office2019-kprometheus.png" },
+     {"WPS PDF", "/usr/bin/wpspdf ", "/usr/share/icons/hicolor/16x16/apps/wps-office2019-pdfmain.png" },
+     {"WPS Presentation", "/usr/bin/wpp ", "/usr/share/icons/hicolor/16x16/apps/wps-office2019-wppmain.png" },
+     {"WPS Spreadsheets", "/usr/bin/et ", "/usr/share/icons/hicolor/16x16/apps/wps-office2019-etmain.png" },
+     {"WPS Writer", "/usr/bin/wps ", "/usr/share/icons/hicolor/16x16/apps/wps-office2019-wpsmain.png" },
+ }
+
+ local menua417aafc9b2f25e6d396aadf7c37ab21 = {
+     {"Cajas", "gnome-boxes "},
+     {"Code - OSS", "/usr/bin/code-oss --unity-launch ", "/usr/share/pixmaps/com.visualstudio.code.oss.png" },
+     {"DBeaver Community", "dbeaver", "/usr/share/icons/hicolor/16x16/apps/dbeaver.png" },
+     {"Electron 12", "electron12 ", "/usr/share/pixmaps/electron12.png" },
+     {"Qt Assistant", "assistant", "/usr/share/icons/hicolor/32x32/apps/assistant.png" },
+     {"Qt Designer", "designer ", "/usr/share/icons/hicolor/128x128/apps/QtProject-designer.png" },
+     {"Qt Linguist", "linguist ", "/usr/share/icons/hicolor/16x16/apps/linguist.png" },
+     {"Qt QDBusViewer ", "qdbusviewer", "/usr/share/icons/hicolor/32x32/apps/qdbusviewer.png" },
+ }
+
+ local menu7dca2558c58f0a1ad29089ca323f9a79 = {
+     {"Control de volumen de PulseAudio", "pavucontrol"},
+     {"Qt V4L2 test Utility", "qv4l2", "/usr/share/icons/hicolor/16x16/apps/qv4l2.png" },
+     {"Qt V4L2 video capture utility", "qvidcap", "/usr/share/icons/hicolor/16x16/apps/qvidcap.png" },
+     {"Reproductor multimedia VLC", "/usr/bin/vlc --started-from-file ", "/usr/share/icons/hicolor/16x16/apps/vlc.png" },
+     {"Strawberry", "strawberry ", "/usr/share/icons/hicolor/48x48/apps/strawberry.png" },
+     {"Volume Icon", "volumeicon"},
+ }
+
+xdgmenu = {
+    {"Accesorios", menuf9c59cf15f7e3fd54a418134ad8a8be6},
+    {"Gráficos", menue2fa0f6ba17f6bf3d1bf1dbed5a1c5b2},
+    {"Herramientas del sistema", menu010ccd73d8965edb946094904b05ace3},
+    {"Internet", menuc8205c7636e728d448c2774e6a4a944b},
+    {"Oficina", menubd41283f290f7ee66eb3f4601b70a182},
+    {"Programación", menua417aafc9b2f25e6d396aadf7c37ab21},
+    {"Sonido y vídeo", menu7dca2558c58f0a1ad29089ca323f9a79},
+}
+
---
+++ awesome/autorun.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+function run {
+   if ! pgrep $1 ;
+   then
+	$@&
+   fi
+}
+
+#screens resolution
+run xrandr --output HDMI1 --mode 1440x900 --rate 59.90 --output eDP1 --mode 1366x768 --rate 60.00 --left-of HDMI1 
+#transparency
+#run picom
+
+#update aplication menu
+run xdg_menu --format awesome --root-menu /etc/xdg/menus/arch-applications.menu > ~/.config/awesome/archmenu.lua
+
+#background screen 1 and 2
+run nitrogen --set-zoom-fill --random ~/.config/wallpapers
+#nitrogen --set-zoom-fill --head=screen2 --random ~/.config/wallpapers
+
+#icon volume
+run volumeicon 
+
+#network manager applet
+run nm-applet 
+
+#xfce4-power manager
+run xfce4-power-manager
+
+#key num on
+run numlockx on
